2b1ec738f4e453c046b78d3c3facf073545fdd4f
Paul Hiduk <59006504+gpaul47@users.noreply.github.com>
2022-05-26 10:37:03 +0300

    Dz25

---
+++ Dz25java src/Animal.java
@@ -0,0 +1,11 @@
+public abstract class Animal {
+
+    abstract void makeSound();
+
+    void run() {
+    }
+
+    Animal() {
+
+    }
+}
---
+++ Dz25java src/Main.java
@@ -0,0 +1,9 @@
+public class Main { //Dz24
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        Cat cat1 = new Cat();
+        dog1.makeSound();
+        cat1.makeSound();
+
+    }
+}
